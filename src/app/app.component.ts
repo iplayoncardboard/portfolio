4bbcb20193cb49fb403f62df79ca65c4c7533868
Erik Hurst <erik_hurst@fsafood.com>
2020-08-08 12:55:30 -0700

    side nav responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component, EventEmitter, ViewChild } from '@angular/core';
+import { Component, EventEmitter, ViewChild, ChangeDetectorRef, OnDestroy } from '@angular/core';
 import { MatDrawer } from '@angular/material/sidenav';
-
+import {MediaMatcher} from '@angular/cdk/layout';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,10 +9,20 @@ import { MatDrawer } from '@angular/material/sidenav';
 export class AppComponent {
   @ViewChild(MatDrawer)
   private matDrawerComponent: MatDrawer;
+  mobileQuery: MediaQueryList;
 
+  private mobileQueryListener: () => void;
+
+  // constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher){
+  //   this.mobileQuery = media.matchMedia('(max-width: 600px)');
+  //   this.mobileQueryListener = () => changeDetectorRef.detectChanges();
+  //   this.mobileQuery.addListener(this.mobileQueryListener);
+  // }
   toggleContactTray(){
     this.matDrawerComponent.toggle();
 
+  // }
+  // ngOnDestroy(): void {
+  //   this.mobileQuery.removeListener(this.mobileQueryListener);
   }
-
 }
